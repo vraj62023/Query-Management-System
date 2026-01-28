04ab9291829dfaf9aba2430fd4a9443c297a9c27
vraj62023 <vraj62023.24kgpian.iitkgp.ac.in>
2026-01-28 11:10:45 +0530

    prepare cors and env for deployment

--- server/index.js
+++ server/index.js
@@ -1,31 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
 const queryRoutes = require('./routes/queryRoutes');
 const adminRoutes = require('./routes/adminRoutes'); 
 
-//load environment variables from .env files
 dotenv.config();
 
 const app = express();
 
-//Middleware to let the server read the JSON data from the frontend
+app.use(cors({
+    origin: [
+        "http://localhost:5173",                 
+        process.env.CLIENT_URL                   
+    ],
+    credentials: true  
+}))                         
+
 app.use(express.json());
 
-app.use('/api/auth',authRoutes); //base pathing
+
+app.use('/api/auth',authRoutes);
 app.use('/api/queries', queryRoutes);
 app.use('/api/admin', adminRoutes);
 
-//basic route to test if server is working 
+
 app.get('/',(req,res)=>{
     res.send('server is running');
 
 });
-//set the port 
+
 const PORT = process.env.PORT || 5000;
 
-//connect to the mongodb and start the server
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log('db is connected');
